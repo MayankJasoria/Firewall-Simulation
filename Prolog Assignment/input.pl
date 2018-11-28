c19db71c3e442f88c32717a38706b4617896c653
sbhtwr <sbhtwr@users.noreply.github.com>
2018-11-28 20:08:43 +0530

    defined root structure

---
+++ Prolog Assignment/input.pl
@@ -0,0 +1,7 @@
+/**
+ * Database of input packets.
+ * Each packet is defined as a list of values 
+ * [Adapter, IPv4 addr, TCP/UDP port, ICMP msg, ICMP code, ipv4 proto]
+ */
+
+packet(1, [A, '172.17.2.11', '80', 'ping', '2', '0x86dd']) .
