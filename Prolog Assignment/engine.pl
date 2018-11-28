e4e02497e136f82c0588dba8381cf20bc28b24f6
sbhtwr <sbhtwr@users.noreply.github.com>
2018-11-28 21:24:29 +0530

    initiated structuring of engine

--- Prolog Assignment/engine.pl
+++ Prolog Assignment/engine.pl
@@ -1,5 +1,20 @@
+/**
+ * Loading inputs and database module.
+ */
+:- ensure_loaded('input.pl').
+:- use_module('input.pl', [packet/2]).
+:- ensure_loaded('database.pl').
+:- use_module('database.pl', [accept_adapter/1]).
 
- % :- consult('input.pl').
- :- ensure_loaded('input.pl').
- :- use_module('input.pl', [packet/2]).
-% consult_File :- consult('E/Prolog-Assignment/Prolog Assignment/input.pl').
+reject(Z) :- nl, write('Packet ID '), write(Z), write(' rejected with a message.').
+accepted(Z) :- nl, write('Packet ID '), write(Z), write(' accepted.').
+dropped(Z) :- nl, write('Packet ID '), write(Z), write(' dropped!').
+
+
+act(X,Y) :- 
+	/* check for adapter id */
+	
+
+ 
+:- forall(packet(X, Y), act(X, Y)).
+
