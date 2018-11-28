badfbd37f4aac67ea5603a031aee8c7ceff473b7
sbhtwr <sbhtwr@users.noreply.github.com>
2018-11-28 21:03:40 +0530

    defined adapter clause in database

--- Prolog Assignment/database.pl
+++ Prolog Assignment/database.pl
@@ -1,4 +1,14 @@
-likes(mary,food).
-likes(mary,wine).
-likes(john,wine).
-likes(john,mary).
+/**
+ * Contains the firewall clauses and conditions in prolog format.
+ * Examlple of how to edit this prolog file has been provided in comments.
+ */
+ 
+/**
+ * Adapter clause format in form of predicates is defined as follows: 
+ * accept_adapter(<Adapter-id>) 
+ * accept_adapter([list_of_comma_sep_adapter_ids]) 
+ * accept_adapter_r(lower_adapter_id, upper_adapter_id)
+ */
+  
+accept_adapter(a).  
+ 
